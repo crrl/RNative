0f3f7b1f67f7fd6d330ec631c773c002d64157b5
christian <christian@MacBook-Pro-de-Desarrollo-2.local>
2019-10-07 10:17:26 -0600

    Primer paso 'instalacion'.

--- Proyecto01/App.js
+++ Proyecto01/App.js
@@ -1,19 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, Component } from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Holi!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
